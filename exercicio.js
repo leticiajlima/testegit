bf21d7b2c25dd7010667e99de2330f52dc6e60ff
LeticiaLima <leticia.jlima97@gmail.com>
2023-12-01 17:18:19 -0300

    atividadesemana1

---
+++ exercicio.js
@@ -0,0 +1,14 @@
+function mostrarCabecalho(){
+    document.write("::Lista de Clientes")
+    
+ }
+    
+function mostrarCabecalho2(nome){
+    document.write(nome)
+}
+
+var nome = "Vitor"
+var nome2 = "Luiz"
+var nome3 = "Regina"
+
+mostrarCabecalho2 (nome + nome2 + nome3)
